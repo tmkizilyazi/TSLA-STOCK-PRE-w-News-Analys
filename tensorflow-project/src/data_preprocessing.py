7e420c88d0fa0cd360d327529558caa2fc8de949
Tuğra Miraç Kızılyazı <41338920+tmkizilyazi@users.noreply.github.com>
2025-03-02 06:03:05 +0300

    Update data_preprocessing.py

--- tensorflow-project/src/data_preprocessing.py
+++ tensorflow-project/src/data_preprocessing.py
@@ -10,7 +10,7 @@ def preprocess_data(data):
     return data[['ds', 'y']]
 
 if __name__ == "__main__":
-    file_path = r'C:\Users\kizilyazitug\Desktop\train\tensorflow-project\data\TSLA_2010-06-29_2025-02-13.csv'
+    file_path = ==='
     raw_data = load_data(file_path)
     processed_data = preprocess_data(raw_data)
     print(processed_data.head())
